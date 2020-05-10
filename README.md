b89c3017f64e7e56b18c708993934fa9de8c68bc
EDusik <eduardodusik@gmail.com>
2020-05-10 18:34:04 -0300

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<div align="center">
+    <img alt="Gostack Bootcamp" title="#GostackBootcamp" src="/images/bootcamp.png" width="250px" />
+</div>
+
+<div align="center">
+  <img alt="Gostack Technologies" src="/images/technologies.jpg" width="50%">
+</div>
+
+<div id="about"> 
+
+## :page_facing_up: About
+Go Stack
+These projects was based on the Rocketseat course called "Go Stack" that you can find in this link: https://rocketseat.com.br/
+</div>
+
+<div id="tecnologies"> 
+
+## :rocket: Technologies
+This project uses these technologies:
+- [Node.js](https://nodejs.org/en/)
+- [React](https://reactjs.org)
+- [React Native](https://facebook.github.io/react-native/)
+<!-- - [Expo](https://expo.io/) -->
+</div>
+
+<div id="directories">   
+
+## :file_folder: Directories
+#### /backend
+Node application
+
+#### /frontend
+React application
+
+#### /mobile
+React Native application
+</div>
---
+++ node/module-01/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+
+// Query Params ?user=1
+// Route Params = /users/1
+// Request Body = {"name": "Eduardo Dusik" ... }
+
+// CRUD - Create, Read, Update, Delete
+
+const users = ['Eduardo', 'Eduardo Dusik'];
+
+server.use((req, res, next) => {
+    console.time('Request Time');
+    console.log(`Method: ${req.method}; URL: ${req.url};`);
+
+    next();
+
+    console.timeEnd('Request Time');
+});
+
+function checkUserExists(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).json({ error: 'User name is required' });
+    }
+
+    return next();
+}
+
+function checkUserInArray(req, res, next) {
+
+    const user = users[req.params.index];
+
+    if (!user) {
+        return res.status(400).json({ error: 'User does not exists' });
+    }
+
+    req.user = user;
+
+    return next();
+}
+
+server.get('/users/', (req, res) => {
+    return res.json(users);
+});
+
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
+    const { name } = req.body;
+    users.push(name);
+    return res.json(users);
+});
+
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+    
+    return res.json(users);
+});
+
+server.delete('/users/:index', checkUserInArray, (req, res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+    
+    return res.send();
+});
+
+server.listen(3000);
