18410efc12b6fa3f5fdf04919be1e63ec43e88d0
EDusik <eduardodusik@gmail.com>
2020-08-24 21:09:03 -0300

    Appointment Controller

--- node/backend-gobarber/src/routes.js
+++ node/backend-gobarber/src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
-import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
-import FileController from './app/controllers/FileController';
-import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import multerConfig from './config/multer';
+import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,7 @@ routes.use(authMiddleware);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 
 routes.get('/providers', ProviderController.index);
 
