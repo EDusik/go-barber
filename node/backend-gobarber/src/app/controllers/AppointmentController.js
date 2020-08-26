d986c222a677d5aaa07a8bbb7063ad5f811343ad
EDusik <eduardodusik@gmail.com>
2020-08-26 18:52:06 -0300

    Send message with nodemail and mailtrap.io

--- node/backend-gobarber/src/app/controllers/AppointmentController.js
+++ node/backend-gobarber/src/app/controllers/AppointmentController.js
@@ -4,6 +4,7 @@ import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import File from '../models/File';
+import Mail from '../../lib/Mail';
 import Notification from '../schemas/Notification';
 import User from '../models/User';
 
@@ -125,7 +126,13 @@ class AppointmentController {
 
   async delete(req, res) {
 
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [{
+        model: User,
+        as: 'provider',
+        attributes: ['name', 'email']
+      }]
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -143,6 +150,12 @@ class AppointmentController {
 
     appointment.canceled_at = new Date;
     await appointment.save()
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      text: 'Você tem um novo cancelamento'
+    });
 
     return res.json(appointment);
   }
---
+++ node/backend-gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "b54adac60316fb",
+    pass: "51e8415021945b"
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>'
+  }
+};
