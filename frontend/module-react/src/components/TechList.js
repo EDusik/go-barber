d7fb21b0426ba38a7ff075d987acecf526a44c39
EDusik <eduardodusik@gmail.com>
2020-09-02 20:17:26 -0300

    Immutability and spread operator

--- frontend/module-react/src/components/TechList.js
+++ frontend/module-react/src/components/TechList.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class TechList extends Component {
     
     state = {
+        newTech: '',
         techs: [
             'Node.js',
             'React.JS',
@@ -10,15 +11,33 @@ class TechList extends Component {
         ]
     };
 
-    render() {
-        return (
-            <ul>
-                <li>Node.js</li>
-                <li>React.js</li>
-                <li>React Native</li>
-            </ul>
-        );
-    }        
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ 
+            techs: [...this.state.techs, this.state.newTech],  
+            newTech: ''
+        });
+    }
+
+    render() { 
+        return (   
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+                <input
+                    type="text"
+                    onChange={this.handleInputChange} 
+                    value={this.state.newTech} 
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        );  
+    }
 }
 
 export default TechList;
