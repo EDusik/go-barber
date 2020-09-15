f87ef52beb38bac849d5a2635e6115cc8c2a515b
EDusik <eduardodusik@gmail.com>
2020-09-15 19:07:40 -0300

    Adding localStorage

--- frontend/frontend-gobarber/src/pages/Main/index.js
+++ frontend/frontend-gobarber/src/pages/Main/index.js
@@ -13,6 +13,24 @@ export default class Main extends Component {
         username: 'EDusik'
     };
 
+    componentDidMount() {
+        const repositories = localStorage.getItem('repositories')
+
+        if (repositories) {
+            this.setState({
+                repositories: JSON.parse(repositories)
+            })
+        }
+
+    }
+
+    componentDidUpdate(_, prevState) {
+        const { repositories } = this.state;
+        if (prevState.repositories !== repositories) {
+            localStorage.setItem('repositories', JSON.stringify(repositories));
+        }
+    }
+
     handleInputChange = e => {
         this.setState({
             newRepo: e.target.value
