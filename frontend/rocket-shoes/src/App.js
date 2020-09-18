dfcb60094cf502707230dcc100832b8ba5035f0b
EDusik <eduardodusik@gmail.com>
2020-09-17 21:21:20 -0300

    Adding GlobalStyle

--- frontend/rocket-shoes/src/App.js
+++ frontend/rocket-shoes/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 
+import GlobalStyles from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes />
+        <GlobalStyles />
     </BrowserRouter>
   );
 }
